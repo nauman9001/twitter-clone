3c34c9f91751e0fef583df14afef0544c947d2cf
Nauman Nazir <nauman.novatore@gmail.com>
2023-04-13 15:02:12 +0500

    code pushed

--- lib/common/error_text.dart
+++ lib/common/error_text.dart
@@ -15,6 +15,6 @@ class ErrorPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text(error),));
+    return Scaffold(body: Center(child: Text(error ),));
   }
 }
